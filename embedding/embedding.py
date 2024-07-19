013b853105b3b8d58ae4b693af87b65f5cdaa92b
Tinh Nguyen <tinhnguyen@chotot.vn>
2024-07-19 21:36:25 +0700

    add embedding

---
+++ embedding/embedding.py
@@ -0,0 +1,37 @@
+from pymilvus import model
+from pymilvus import MilvusClient
+
+
+embedding_fn = model.DefaultEmbeddingFunction()
+
+docs = [
+    "Artificial intelligence was founded as an academic discipline in 1956.",
+    "Alan Turing was the first person to conduct substantial research in AI.",
+    "Born in Maida Vale, London, Turing was raised in southern England.",
+]
+
+vectors = embedding_fn.encode_documents(docs)
+print("Dim:", embedding_fn.dim, vectors[0].shape)  # Dim: 768 (768,)
+
+data = [
+    {"id": i, "vector": vectors[i], "text": docs[i], "subject": "history"}
+    for i in range(len(vectors))
+]
+
+print("Data has", len(data), "entities, each with fields: ", data[0].keys())
+print("Vector dim:", len(data[0]["vector"]))
+
+# 1. Set up a Milvus client
+client = MilvusClient(
+    uri="http://localhost:19530"
+)
+
+# 2. Create a collection
+client.create_collection(
+    collection_name="demo_collection",
+    dimension=2304,
+)
+
+res = client.insert(collection_name="demo_collection", data=data)
+
+print(res)
